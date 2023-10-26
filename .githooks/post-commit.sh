680c40b87da8dae297c6eddaf69afd0f7416a288
Erik Pöhler <erik.poehler@fork.de>
2023-10-26 22:40:41 +0200

    chore: comments

--- .githooks/post-commit.sh
+++ .githooks/post-commit.sh
@@ -6,6 +6,6 @@ if test -f "$FILE"; then
     MODIFIED_FILES=$(cat .commit)
     echo "Amending fixed .php files ($MODIFIED_FILES) to the current commit."
     rm $FILE # and removing temp file
-    git add $MODIFIED_FILES # adding fixed files to the staging area
+    git add $MODIFIED_FILES # adding all (incl. fixed) files to the staging area
     git commit --amend -C HEAD --no-verify # --no-verify to avoid running pre-commit hook again
 fi
