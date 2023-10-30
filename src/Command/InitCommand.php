767b237d946cff125e8e3ecdab3041051bebd0d5
Erik Pöhler <erik.poehler@fork.de>
2023-10-30 13:41:31 +0100

    fix: update dependencies, fix namepsace error in command

--- src/Command/InitCommand.php
+++ src/Command/InitCommand.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Command;
+namespace DOM\ORM\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
