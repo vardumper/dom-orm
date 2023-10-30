1d7367dbf460f53eb8991239128b991ac6692e8c
Erik Pöhler <erik.poehler@fork.de>
2023-10-30 19:30:18 +0100

    chore: console init command

---
+++ src/Command/Init.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace DOM\ORM\Command;
+
+class Init
+{
+    protected static $defaultName = 'dom-orm:init';
+
+    public static function run($path): void
+    {
+        $storage = $path . '/storage/data.xml';
+
+        if (!\is_dir(\dirname($storage))) {
+            \mkdir(\dirname($storage), 0755, true);
+        }
+
+        if (!\is_writable(\dirname($storage))) {
+            \chmod(\dirname($storage), 0755);
+        }
+
+        if (!\file_exists($storage)) {
+            $dom = new \DOMDocument('1.0', 'utf-8');
+            $dom->preserveWhiteSpace = false;
+            $dom->validateOnParse = false;
+            $dom->strictErrorChecking = false;
+            $dom->formatOutput = true;
+            $dom->loadXML('<data />');
+            $dom->save($storage);
+        }
+    }
+}
