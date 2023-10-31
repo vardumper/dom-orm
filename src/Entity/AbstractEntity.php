5200119fcb6203ab6f3d77ea680b24b763896549
Erik Pöhler <erik.poehler@fork.de>
2023-10-31 13:25:33 +0100

    chore: make abstract class non-abstract

--- src/Entity/AbstractEntity.php
+++ src/Entity/AbstractEntity.php
@@ -9,7 +9,7 @@ use DOM\ORM\Traits\AttributeResolverTrait;
 use Ramsey\Collection\Collection;
 use Ramsey\Uuid\Uuid;
 
-class AbstractEntity implements EntityInterface
+abstract class AbstractEntity implements EntityInterface
 {
     use AttributeResolverTrait;
 
