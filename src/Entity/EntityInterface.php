ad072e5a07bc5f02a4cdd4ddf3d7611ffb8a1296
Erik Pöhler <iroybot@gmail.com>
2024-06-23 11:20:16 +0200

    chore: add setters

--- src/Entity/EntityInterface.php
+++ src/Entity/EntityInterface.php
@@ -8,13 +8,15 @@ interface EntityInterface
 {
     public function getId(): string;
 
-    public function setId($id): void;
+    public function setId(string $id): void;
 
     public function getDeletedAt(): ?\DateTimeInterface;
 
     public function setDeletedAt(\DateTimeInterface $deletedAt): static;
 
     public function getCreatedAt(): \DateTimeInterface;
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): void;
 
     public function getUpdatedAt(): ?\DateTimeInterface;
 
