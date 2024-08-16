9e510b95a6167554b31a13ff5a165950c220ce55
Erik Pöhler <erik.poehler@fork.de>
2024-08-16 13:29:57 +0200

    chore: pending/staged commit

--- src/Serializer/Encoder/SchemaEncoder.php
+++ src/Serializer/Encoder/SchemaEncoder.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace DOM\ORM\Serializer\Encoder;
 
+use Symfony\Component\Serializer\Encoder\DecoderInterface;
 use Symfony\Component\Serializer\Encoder\EncoderInterface;
 
 class SchemaEncoder implements EncoderInterface
@@ -24,8 +25,16 @@ class SchemaEncoder implements EncoderInterface
         return $format === self::FORMAT;
     }
 
-    public function encode($data, string $format = null, array $context = []): string
+    /**
+     * Encodes an array of data into a XML string.
+     * @param mixed  $data    Data to encode
+     * @param string $format  Format name
+     * @param array  $context Options that normalizers/encoders have access to
+     * @throws \InvalidArgumentException
+     */
+    public function encode(mixed $data, string $format = null, array $context = []): string
     {
+        // we only support arrays here
         if (!is_array($data)) {
             throw new \InvalidArgumentException('Only arrays are supported.');
         }
@@ -77,5 +86,120 @@ class SchemaEncoder implements EncoderInterface
 
         // convert to XML string and return
         return $this->dom->saveXML();
+    }
+
+    /**
+     * Decodes an entire XML Document or single DOMElement nodes into an array, which servers to de-serialize it back to objects.
+     * /**
+     * Decodes a string into PHP data.
+     *
+     * @param string $data    Data to decode
+     * @param string $format  Format name
+     * @param array  $context Options that decoders have access to
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function decode(string|\DOMDocument|\DOMElement $data, string $format, array $context = []): mixed
+    {
+        $return = null;
+
+        // if a XML string is passed, load it into an empty DOM
+        if (\is_string($data)) {
+            $isXml = (\simplexml_load_string($data) !== false);
+            if (!$isXml) {
+                throw new \UnexpectedValueException('Only XML strings are supported or DOMDocument is supported.');
+            }
+            $xml = $data;
+            $data = new \DOMDocument();
+            $data->loadXML($xml);
+        }
+
+        // Make sure that $data is DOMDocument or DOMElement
+        if (!$data instanceof \DOMDocument && !$data instanceof \DOMElement) {
+            throw new \InvalidArgumentException('Only an XML string, a DOMElement or a DOMDocument is supported.');
+        }
+
+        // If a DOMDocument is passed, validate against XSD
+        if ($data instanceof \DOMDocument) {
+            // only run schea validation against DOMDocument
+            if (!$data->schemaValidate(__DIR__ . '/../../Resources/schema/schema.xsd')) {
+                throw new \InvalidArgumentException('The XML document does not comply to the schema.xsd');
+            }
+        }
+
+        // If a DOMElement is passed, load into empty DOM
+        if ($data instanceof \DOMElement) {
+            $xml = $data;
+            $data = new \DOMDocument();
+            $importedNode = $data->importNode($xml, true);
+            $data->appendChild($importedNode);
+        }
+
+        $rootNodeName = $data->documentElement->nodeName;
+
+        match ($rootNodeName) {
+            'data' => $return = $this->decodeData($data, $format, $context),
+            'group' => $return = $this->decodeGroup($data, $format, $context),
+            'item' => $return = $this->decodeItem($data, $format, $context),
+            'fragment' => $return = $this->decodeFragment($data, $format, $context),
+            default => throw new \InvalidArgumentException(sprintf('Unsopperted element %s given. Supported elements are data, group, item and fragment.', $rootNodeName)),
+        };
+
+        return $return;
+    }
+
+    private function decodeData($data, $format, $context): ?array
+    {
+        $tmp = [];
+        foreach ($data->documentElement->childNodes as $child) {
+            $tmp[] = $this->decode($child, $format, $context);
+        }
+
+        return [
+            'data' => $tmp,
+        ];
+    }
+
+    private function decodeGroup($data, $format, $context): ?array
+    {
+        $groupType = $data->documentElement->getAttribute('type');
+        $groupItems = [];
+        foreach ($data->documentElement->childNodes as $child) {
+            $groupItems = $this->decode($child, $format, $context);
+        }
+
+        return [
+            $groupType => $groupItems,
+        ];
+    }
+
+    private function decodeItem($data, $format, $context): ?array
+    {
+        $id = $data->documentElement->getAttribute('id');
+
+        $itemData = [
+            '@id' => $id,
+            '@type' => $data->documentElement->getAttribute('type'),
+            // '@class' => $data->documentElement->getAttribute('class'), not really needed, we find the class by its attribute
+        ];
+
+        // merge each child node into itemData
+        foreach ($data->documentElement->childNodes as $child) {
+            $itemData = array_merge($itemData, $this->decode($child, $format, $context));
+        }
+
+        return [
+            'item-' . $id => $itemData,
+        ];
+    }
+
+    private function decodeFragment($data, $format, $context): ?array
+    {
+        $name = $data->documentElement->getAttribute('name');
+        $value = $data->documentElement->nodeValue;
+
+        return [
+            $name => $value,
+        ];
     }
 }
