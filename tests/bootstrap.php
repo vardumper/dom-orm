d84e878a42c0926f5c1fbee6968a042144fb3324
Erik Pöhler <admin@teuton.mx>
2026-04-24 09:44:54 +0200

    fix: autoload fixtures

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,3 +2,12 @@
 declare(strict_types=1);
 
 require dirname(__DIR__) . '/vendor/autoload.php';
+
+// Force PSR-4 autoloading of all test fixture classes so that
+// AttributeResolverTrait::warmUpReflectionCache() can discover their
+// #[ORM\Item] entityType attributes via get_declared_classes().
+foreach (new \DirectoryIterator(__DIR__ . '/Fixtures') as $file) {
+    if ($file->isFile() && $file->getExtension() === 'php') {
+        class_exists('Tests\\Fixtures\\' . $file->getBasename('.php'));
+    }
+}
